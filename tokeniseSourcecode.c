96ba42b4046ac93e2f3846450d252d03a6d270b3
ChiragCD <chirag8cd@gmail.com>
2020-10-22 15:49:41 +0530

    Add functions

--- tokeniseSourcecode.c
+++ tokeniseSourcecode.c
@@ -5,6 +5,149 @@ int is_whitespace(char * source) {
     return 0;
 }
 
+void read_symbol(char *buf, tokenStream *s, int *read) {
+    if(*read) return;
+
+    if(buf[0] == '(') {
+        s->token = BO;
+        *read = 1;
+    }
+    if(buf[0] == ')') {
+        s->token = BC;
+        *read = 1;
+    }
+    if(buf[0] == '[') {
+        s->token = SQO;
+        *read = 1;
+    }
+    if(buf[0] == ']') {
+        s->token = SQC;
+        *read = 1;
+    }
+    if(buf[0] == '{') {
+        s->token = CURLO;
+        *read = 1;
+    }
+    if(buf[0] == '}') {
+        s->token = CURLC;
+        *read = 1;
+    }
+    if(buf[0] == ':') {
+        s->token = CLN;
+        *read = 1;
+    }
+    if(buf[0] == ';') {
+        s->token = SEMCOL;
+        *read = 1;
+    }
+    if(buf[0] == '..') {
+        s->token = ROP;
+        *read = 1;
+    }
+    if(buf[0] == '=') {
+        s->token = ASSGN;
+        *read = 1;
+    }
+    if(buf[0] == '+') {
+        s->token = OP_PLUS;
+        *read = 1;
+    }
+    if(buf[0] == '-') {
+        s->token = OP_MINUS;
+        *read = 1;
+    }
+    if(buf[0] == '*') {
+        s->token = OP_STAR;
+        *read = 1;
+    }
+    if(buf[0] == '/') {
+        s->token = OP_SLASH;
+        *read = 1;
+    }
+    if(strcmp(buf, "|||") == 0) {
+        s->token = OP_OR;
+        *read = 3;
+    }
+    if(strcmp(buf, "&&&") == 0) {
+        s->token = OP_AND;
+        *read = 3;
+    }
+}
+
+void read_number(char *buf, tokenStream *s, int *read) {
+    if(*read) return;
+
+    int success = 1;
+    int len = strlen(buf);
+    for(int i = 0; i < len; i++) if(!(buf[i] >= '0' && buf[i] <= '9')) success = 0;
+    if(!success) return;
+
+    *read = len;
+    s->token = INT;
+}
+
+void read_keyword(char *buf, tokenStream *s, int *read) {
+    if(*read) return;
+
+    int success = 0;
+    if(strcmp(buf, "program") == 0) {
+        s->token = KEY_PROG;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "declare") == 0) {
+        s->token = KEY_DECL;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "list") == 0) {
+        s->token = KEY_LIST;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "of") == 0) {
+        s->token = KEY_OF;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "variables") == 0) {
+        s->token = KEY_VARS;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "array") == 0) {
+        s->token = KEY_ARR;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "r1") == 0) {
+        s->token = R1;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "size") == 0) {
+        s->token = KEY_SIZE;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "vals") == 0) {
+        s->token = KEY_VALS;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "jag") == 0) {
+        s->token = KEY_JAG;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "int") == 0) {
+        s->token = KEY_INT;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "real") == 0) {
+        s->token = KEY_REAL;
+        *read = strlen(buf);
+    }
+    if(strcmp(buf, "bool") == 0) {
+        s->token = KEY_BOOL;
+        *read = strlen(buf);
+    }
+}
+
+void read_id(char *buf, tokenStream *s, int *read) {
+    if(*read) return;
+}
+
 void tokeniseSourcecode(char * source_file, tokenStream *s) {
     long size;
     char * source = loadfile(source_file, &size);
@@ -37,13 +180,14 @@ void tokeniseSourcecode(char * source_file, tokenStream *s) {
             temp++;
         }
         buf[temp] = '\0';
+        strcpy(s->lexeme, buf);
+        s->line_num = line_num;
         if(temp == 20) {
             printf("Aborting - Lexeme %s... at line %d too long\n", buf, line_num);
             exit(0);
         }
 
-        read_single_symbol(buf, s, &read);
-        read_multi_symbol(buf, s, &read);
+        read_symbol(buf, s, &read);
         read_number(buf, s, &read);
         read_keyword(buf, s, &read);
         read_id(buf, s, &read);
@@ -54,10 +198,10 @@ void tokeniseSourcecode(char * source_file, tokenStream *s) {
         }
         source += read;
         s->next = (tokenStream *) malloc(sizeof(tokenStream));
+        s = s->next;
     }
 
     s->token = END;
-    s->lexeme = NULL;
     s->line_num = line_num;
     s->next = NULL;
 }
