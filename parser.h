aa27623b437db30abefea64f8229d2bb6d3f18e5
ChiragCD <chirag8cd@gmail.com>
2020-10-22 14:47:12 +0530

    Add general header

---
+++ parser.h
@@ -0,0 +1,19 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+typedef enum Token {
+    BO, BC, SQO, SQC, CURLO, CURLC, CLN, SEMCOL, ROP, ASSGN,
+
+    KEY_PROG, KEY_DECL, KEY_LIST, KEY_OF, KEY_VARS, KEY_ARR, R1,
+    KEY_SIZE, KEY_VALS, KEY_JAG, KEY_INT, KEY_REAL, KEY_BOOL,
+
+    OP_PLUS, OP_MINUS, OP_STAR, OP_SLASH, OP_AND, OP_OR,
+
+    ID, INT
+} Token;
+
+#endif
