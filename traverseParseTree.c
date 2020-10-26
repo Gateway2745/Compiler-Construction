210616c0259f4f7f2d8d319a2674008a00db1baa
ChiragCD <chirag8cd@gmail.com>
2020-10-26 21:46:22 +0530

    Jagged 2

--- traverseParseTree.c
+++ traverseParseTree.c
@@ -26,7 +26,52 @@ link * run_primitive(parseTree * node) {
 }
 
 link * run_jagged(parseTree * dec, parseTree * init) {
-    ;
+    link * info = (link *) malloc(sizeof(link));
+    info->arr_info = JAG_ARR;
+    info->arr_storage = NONE;
+    info->type.jagged_arr_info.betype = INTEGER;
+    info->type.jagged_arr_info.num_dim = (dec->term.type.nt == JAGGED2_DEC) ? 2 : 3;
+    int dim2 = (info->type.jagged_arr_info.num_dim == 2);
+    int x, y;
+    parseTree * x_node = dec->children[3]->children[0]->children[0];
+    parseTree * y_node = dec->children[3]->children[2]->children[0];
+
+    if(x_node->term.type.tok.token == ID || y_node->term.type.tok.token == ID) {
+        printf("Error - Line %d - Jagged arrays cannot have dynamic bounds\n", x_node->term.type.tok.line_num);
+        return NULL;
+    }
+    info->type.jagged_arr_info.range_R1.r1 = atoi(x_node->term.type.tok.lexeme);
+    info->type.jagged_arr_info.range_R1.r2 = atoi(y_node->term.type.tok.lexeme);
+    if(info->type.jagged_arr_info.range_R1.r1 > info->type.jagged_arr_info.range_R1.r2) {
+        printf("Error - Line %d - Lower bound cannot exceed upper bound\n", x_node->term.type.tok.line_num);
+        return NULL;
+    }
+
+    int range_1 = 1 + info->type.jagged_arr_info.range_R1.r2 - info->type.jagged_arr_info.range_R1.r1;
+    info->type.jagged_arr_info.range_R2 = (rng_R2 *) malloc(range_1 * sizeof(rng_R2));
+    
+    if(dim2) {
+        for(int i = 0; i < range_1; i++) {
+            info->type.jagged_arr_info.range_R2[i].num_dim = 1;
+            info->type.jagged_arr_info.range_R2[i].dims = (int *) malloc(sizeof(int));
+        }
+        int count = 0;
+        while(count < range_1 && init->children[0]->term.is_term == 0 && init->children[0]->term.type.nt == JAGGED2_SINGLE_INIT) {
+            info->type.jagged_arr_info.range_R2[count++].dims[0] = atoi(init->children[0]->children[6]->term.type.tok.lexeme);
+            if(init->num_children > 1) init = init->children[1];
+            else break;
+        }
+        if(count < range_1 || init->num_children > 1) {
+            printf("Error - Line %d - Size mismatch\n", init->children[0]->children[6]->term.type.tok.line_num);
+            return NULL;
+        }
+        return info;
+    }
+    
+    for(int i = 0; i < range_1; i++) {
+        info->type.jagged_arr_info.range_R2[i].num_dim = 1;
+        info->type.jagged_arr_info.range_R2[i].dims = (int *) malloc(sizeof(int));
+    }
 }
 
 link * get_type(parseTree * tree, int is_single) {
