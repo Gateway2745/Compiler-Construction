9fb7d51d5a262bc960d987129325a733fa4938fb
ChiragCD <chirag8cd@gmail.com>
2020-10-26 20:02:57 +0530

    Start traverser

--- traverseParseTree.c
+++ traverseParseTree.c
@@ -1,5 +1,23 @@
 #include "parser.h"
 
+link * copy_link(link * source) {
+    link * new = (link *) malloc(sizeof(link));
+    new->arr_info = source->arr_info;
+    new->arr_storage = source->arr_storage;
+    new->type = new->type;
+    return new;
+}
+
+void add_info(parseTree * node, link * info, typeExpressionTable * table) {
+    strcpy(info->id, node->children[0]->term.type.tok.lexeme);
+    put_link(table, info);
+    if(node->num_children > 1) add_info(node->children[1], copy_link(info), table);
+}
+
+link * get_type(parseTree * tree) {
+    ;
+}
+
 void traverseDeclares(parseTree * tree, typeExpressionTable * table) {
     if(!(tree->term.is_term == 0 && tree->term.type.nt == DECL_STMT)) {
         for(int i = 0; i < tree->num_children; i++) traverseDeclares(tree->children[i], table);
@@ -7,6 +25,13 @@ void traverseDeclares(parseTree * tree, typeExpressionTable * table) {
     }
     int single = (tree->children[0]->term.type.nt == SINGLEVAR_DEC);
     tree = tree->children[0];
+
+    link * info = get_type(tree);
+    if(single) {
+        strcpy(info->id, tree->children[1]->term.type.tok.lexeme);
+        put_link(table, info);
+    }
+    else add_info(tree->children[4], info, table);
 }
 
 void traverseParseTree(parseTree *t, typeExpressionTable *Table) {
