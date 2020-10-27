9d427ed41b215f9250ac4230c67b3181a5dd1368
Abhishek Agarwal <42578847+abhiagarwal18@users.noreply.github.com>
2020-10-27 18:24:03 +0530

    Please kill me

--- try.c
+++ try.c
@@ -15,24 +15,25 @@ if(topstack->is_term ==1) {
 }  	// readterm does (topstack->tok == tok_stream->[0] )? 1:0 //pop from stack if success, else not
 stackNode top_stack = peek(stack);
 Term** rules = get_rules(G, top_stack->nonterm, &size);
+
 for(Term * rule = NULL, *rule =  ){ //help pls
-Push rule on temp_stack
-temp_stack = stack(size, rule);
-Cursor * Temp_current_stream_token = *current_token_stream
-Tree_setup (create only for non-terms)
-While temp_stack {
-Is_term = check_if_term(top_temp_stack)
-Success = try(&temp_current_stream_token, tree->child[counter], temp_stack);
-if(!is_term) counter++;
-if(success == 0) break;
-}
-if(sucess){
-Pop from stack
-*Current_token_stream = Temp_current_stream_token
- Break
-}
-tree_destroy
-}
+	temp_stack = stack(size, rule);
+	cursor * temp_current_stream_token = *current_token_stream;
+	Tree_setup (create only for non-terms)// sos
+
+	while (temp_stack->head != NULL) {
+		int is_term = peek(temp_stack)->is_term;
+		if (is_term==1)  try(&temp_current_stream_token, tree->child[counter], temp_stack);
+		if(!is_term) counter++;
+		if(success == 0) break;
+	}
+	if(sucess){
+		pop(stack);
+		*Current_token_stream = temp_current_stream_token;
+		break;
+	}
+	tree_destroy
+	}
 }
 
 
