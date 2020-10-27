6c226a9f33b2526afdb1aba2b8266e6691cd4a05
Abhishek Agarwal <42578847+abhiagarwal18@users.noreply.github.com>
2020-10-27 18:18:15 +0530

    Update try.c

--- try.c
+++ try.c
@@ -5,18 +5,19 @@
 
 
 int try(cursor ** current_token_stream, tree, stack) {
-int size
+int size = ? //malloc what size?
 if(topstack->is_term ==1) {
 	if(topstack->tok == tok_stream->[0])
-		pop(stack);
+		{pop(stack); 
+			return;}
 	else continue;
 
-} return; 	// readterm does (topstack->tok == tok_stream->[0] )? 1:0 //pop from stack if success, else not
+}  	// readterm does (topstack->tok == tok_stream->[0] )? 1:0 //pop from stack if success, else not
 stackNode top_stack = peek(stack);
 Term** rules = get_rules(G, top_stack->nonterm, &size);
-for(Term * rule = NULL, *rule =  )
-For rule in range size {
+for(Term * rule = NULL, *rule =  ){ //help pls
 Push rule on temp_stack
+temp_stack = stack(size, rule);
 Cursor * Temp_current_stream_token = *current_token_stream
 Tree_setup (create only for non-terms)
 While temp_stack {
